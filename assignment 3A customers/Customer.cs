72e3fc798219dcaccb305d80ed5987192b51edc2
Armand Alexandru Balint <70021545+Zedpaixd@users.noreply.github.com>
2022-11-27 19:11:05 +0200

    Add files via upload

---
+++ assignment 3A customers/Customer.cs
@@ -0,0 +1,18 @@
+﻿using MyApp;
+using System;
+
+public class Customer
+{
+
+	public string FullName;
+	public int Age;
+	public string Email;
+
+	public Customer(string fullName, int age, string email)
+	{
+		FullName = fullName;
+		Age = age;
+		Email = email;
+	}
+
+}
---
+++ assignment 3A customers/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static bool IsValidEmail(string strIn)
+        {
+            return Regex.IsMatch(strIn, @"^([\w-\.+]+)@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(]?)$"); //abomination
+        }
+
+        static bool isName(string strIn)
+        {
+            return Regex.IsMatch(strIn, "^[A-Za-z ]{3,}$");
+        }
+
+        static void CreateCustomer()
+        {
+
+            string? fullname,email;
+            int age;
+
+            while (true)  // i hate myself for doing it like this
+            {
+                Console.WriteLine("Input a name: ");
+                fullname = Console.ReadLine();
+                if (fullname != null && isName(fullname)) break;
+                else Console.WriteLine("\nBad name\n");
+            }
+            while (true)
+            {
+                Console.WriteLine("Input an age: ");
+                try
+                {
+                    age = int.Parse(Console.ReadLine());
+                    if (age < 0) throw new Exception("Age can't be negative");
+                    break;
+                }
+                catch (Exception e)
+                { 
+                    Console.WriteLine(string.Format("\n{0}\n",e.Message));
+                }
+            }
+            while (true)
+            {
+                Console.WriteLine("Input an email: ");
+                try
+                {
+                    email = Console.ReadLine();
+                    if (!IsValidEmail(email)) throw new Exception("Bad email");
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(string.Format("\n{0}\n", e.Message));
+                }
+            }
+
+            Customer custommer = new Customer(fullname, age, email);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi uh.. what do u wanna do: \n1. Enter customer \n2. Exit\nOption:");
+            string? option = Console.ReadLine();
+            while (option != "2")
+            {
+                if (option == "1")
+                {
+                    CreateCustomer();
+                }
+                Console.WriteLine("\n What about now? \n1. Enter customer \n2. Exit\nOption:");
+                option = Console.ReadLine();
+            }
+        }
+    }
+}
